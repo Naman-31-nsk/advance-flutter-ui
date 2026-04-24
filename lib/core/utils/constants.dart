1ed3e3e835d5b019389eb744ef654baa68a7767d
Naman-31-nsk <codebreakernaman@gmail.com>
2026-04-24 16:28:28 +0530

    Create constants.dart

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,18 @@
+class AppConstants {
+  AppConstants._();
+
+  static const double paddingSmall = 8.0;
+  static const double paddingMedium = 16.0;
+  static const double paddingLarge = 24.0;
+  static const double paddingXL = 32.0;
+
+  static const double radiusSmall = 8.0;
+  static const double radiusMedium = 12.0;
+  static const double radiusLarge = 20.0;
+
+  static const Duration animFast = Duration(milliseconds: 200);
+  static const Duration animNormal = Duration(milliseconds: 400);
+  static const Duration animSlow = Duration(milliseconds: 700);
+
+  static const String appName = 'GlassUI App';
+}
